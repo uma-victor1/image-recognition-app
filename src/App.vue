43383f3153685ff0894e52486e823a9779f5a521
uma-victor1 <umavictor11@gmail.com>
2021-05-23 22:37:11 +0100

    open camera and take snapshot

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,24 @@
   <div class="w-3/4 m-auto">
     <div>
       <h1 class="text-2xl text-green-800">Object detection with Tensorflow</h1>
+      <div v-if="!isStreaming">
+        <button @click="openCamera">Open Camera</button>
+      </div>
+       <div v-else class="flex justify-between">
+              <button
+                @click="stopStreaming"
+                class="w-32 rounded shadow-md bg-gradient-to-r from-blue-800 to-indigo-800 text-white px-2 py-1"
+              >
+                Stop Streaming
+              </button>
+              <button
+                @click="snapshot"
+                class="w-32 rounded shadow-md bg-gradient-to-r from-blue-800 to-indigo-800 text-white px-2 py-1"
+              >
+                Snapshot
+              </button>
+            </div>
+      <video ref="videoRef" autoplay="true" width="100" id="video"/>
       <div class="bg-gray-300 h-64 w-64 rounded-lg shadow-md bg-cover bg-center">
         <img class="w-64" ref="imgRef" src="https://images.unsplash.com/photo-1503792501406-2c40da09e1e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=752&q=80"
          alt="sciscors" crossorigin="anonymous">
@@ -20,11 +38,16 @@ import { defineComponent } from 'vue';
 require('@tensorflow/tfjs-backend-cpu')
 require('@tensorflow/tfjs-backend-webgl')
 const cocoSsd = require('@tensorflow-models/coco-ssd')
+const video = document.getElementById("video") as HTMLVideoElement;
+const img = document.getElementById("video") as HTMLImageElement;
 export default defineComponent({
   name: 'App',
   setup(){
-    const imgRef = ref("");
-     const result = ref([]);
+    const imgRef = ref(img);
+
+    const videoRef = ref<HTMLVideoElement>(video);
+    const isStreaming = ref(false);
+    const result = ref([]);
 
     async function detect() {
       const img = imgRef.value;
@@ -33,11 +56,37 @@ export default defineComponent({
       result.value = predictions;
       console.log(predictions, img);
     }  
+    async function openCamera() {
+      if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({video: true}).then((stream)=>{
+           isStreaming.value = true;
+          videoRef.value.srcObject = stream;
+        })
+      }
+    }
+    function stopStreaming() {
+      const stream = videoRef.value.srcObject as MediaStream;
+      const tracks = stream.getTracks();
+      tracks.map((track) => track.stop());
+      isStreaming.value = false;
+    }
+    function snapshot() {
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+      ctx.drawImage(videoRef.value, 0, 0, 200, 200);
+      const data = canvas.toDataURL("image/png");
+      imgRef.value.setAttribute("src", data);
+    }
   
     return {
       imgRef,
       detect,
-      result
+      result,
+      isStreaming,
+      videoRef,
+      openCamera,
+      stopStreaming,
+      snapshot,
     }
   }
 });
